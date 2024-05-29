beb0b388927d0c41fc5fd9ed5e8fba4e27779268
prajit96 <santraprajit@gmail.com>
2024-05-29 21:12:22 +0530

    all data showing & UI part also done

---
+++ boxigo-app/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import MyMoves from './components/MyMoves';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="*"
+          element={
+            <Sidebar>
+              <Routes>
+                <Route path="/my-moves" element={<MyMoves />} />
+                {/* Add routes for other components like MyProfile, GetQuote, Logout */}
+              </Routes>
+            </Sidebar>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
