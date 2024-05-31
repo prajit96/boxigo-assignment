aa6b2272982b593aa100f9fc49395c8c1b505872
prajit96 <santraprajit@gmail.com>
2024-05-31 19:53:54 +0530

    mobile responsiveness added

--- boxigo-app/src/App.js
+++ boxigo-app/src/App.js
@@ -13,7 +13,6 @@ const App = () => {
             <Sidebar>
               <Routes>
               <Route path="/" element={<MyMoves />} />
-              
               </Routes>
             </Sidebar>
           }
